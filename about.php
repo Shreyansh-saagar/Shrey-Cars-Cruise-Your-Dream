05ab4df585fdc6295b5e94c43ba23a8076b4f5bd
shreyansh saagar <shreyanshsaagar@gmail.com>
2024-03-13 22:57:16 +0530

    New changes

--- about.php
+++ about.php
@@ -14,7 +14,6 @@ error_reporting(0);
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
-
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
 </head>
 <body>
